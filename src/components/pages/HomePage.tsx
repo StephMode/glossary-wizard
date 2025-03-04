a5a13507c9983c97f2c21aebdb42a9a0b0ff65ce
Stephan Model <stephan.model@outlook.de>
2025-03-04 10:49:27 +0100

    test: finalize writing unit tests, clean up code

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -3,16 +3,6 @@
 export default function HomePage() {
 
     return(
-        <>
-        <nav aria-labelledby='top-nav'></nav>
-        <h1 aria-label='h' aria-labelledby='he'>la</h1>
-        <h1 aria-label='he' aria-labelledby='help'>lala</h1>
-        <p>asdf</p>
-        <ul aria-labelledby='list'>
-            <li>a</li>
-            <li>b</li>
-            <li>c</li>
-        </ul>
-        </>
+        <h1>Glossary Wizard</h1>
     )
 }
