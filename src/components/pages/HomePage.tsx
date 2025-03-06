56d97737a831dbacaedeeb553ef4dd3a0f96b8b6
Stephan Model <stephan.model@outlook.de>
2025-03-06 11:09:06 +0100

    feat: write jsx templating for HomePage content, add first a11y info

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -3,6 +3,38 @@
 export default function HomePage() {
 
     return(
-        <h1>Glossary Wizard</h1>
+        <>
+        <header>
+            <p>Logo</p>
+            <nav aria-label="main-navigation">
+                <ul>
+                    <li><button>Edit/Read Mode</button></li>
+                    <li>About</li>
+                    <li>GitHub</li>
+                </ul>
+            </nav>
+        </header>
+        <div>
+        <nav>
+            <ul>
+                <li>
+                    <h3>Preliminary Definitions</h3>
+                    <h4>Basic Defintions</h4>    
+                </li>
+            </ul>
+        </nav>
+        <main>
+            <h1>Project Glossary</h1>
+            <h2>Preliminary Definitions</h2>
+            <h3>Basic Defintions</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tempora neque sapiente illum repellat architecto reiciendis ea nulla tempore dolorum necessitatibus quos, quasi, earum, iure cupiditate sunt modi voluptatem odio!</p>
+        </main>
+        <aside>
+            <h4>Preliminary Definitions</h4>
+            <h5>Basic Defintions</h5>
+        </aside>
+        </div>
+        <footer></footer>
+        </>
     )
 }
