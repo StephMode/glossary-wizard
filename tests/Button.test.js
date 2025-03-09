bc66c655bf675df0668fcbe22162ab91eb4fdf00
Stephan Model <stephan.model@outlook.de>
2025-03-09 17:45:52 +0100

    test: refine unit test to better reflect context of test case

--- tests/Button.test.js
+++ tests/Button.test.js
@@ -2,25 +2,30 @@ import Button from "../src/components/ui/Button";
 import { render, screen } from "@testing-library/react";
 
 describe(`Unit Test for ${Button.name} component`, () => {
-  const toggleButtonText = "Edit/Read Mode";
+  describe(`based on the ${Button.name} for toggling read and edit mode`, () => {
+    const toggleButtonText = "Edit/Read Mode";
 
-  beforeEach(() => {
-    render(
-      <Button theme={"main-theme"} buttonRole={toggleButtonText}>
-        {toggleButtonText}
-      </Button>
-    );
-  });
+    beforeEach(() => {
+      render(
+        <Button theme={"main-theme"} buttonRole={toggleButtonText}>
+          {toggleButtonText}
+        </Button>
+      );
+    });
 
-  test("Renders an element of role button.", () => {
-    expect(screen.getByRole("button")).toBeInTheDocument();
-  });
+    test("if an element of role button is rendered.", () => {
+      const button = screen.getByRole("button");
+      expect(button).toBeInTheDocument();
+    });
 
-  it("Has text-content according to the role of the button.", () => {
-    expect(screen.getByRole("button").textContent).toBe(toggleButtonText);
-  });
+    test("if it displays the correct text-content.", () => {
+      const button = screen.getByRole("button");
+      expect(button.textContent).toBe(toggleButtonText);
+    });
 
-  it("Has a primary theme.", () => {
-    expect(screen.getByRole("button")).toHaveClass("main-theme");
+    test("it the primary theme is applied.", () => {
+      const button = screen.getByRole("button");
+      expect(button).toHaveClass("main-theme");
+    });
   });
 });
