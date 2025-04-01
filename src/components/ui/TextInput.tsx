45b517cd2b3cd7d34f996858b4f4ab5d00b35905
Stephan Model <stephan.model@outlook.de>
2025-04-01 15:29:18 +0200

    feat: start developing conditional logic and add styling for custom error validation in TextInput component

--- src/components/ui/TextInput.tsx
+++ src/components/ui/TextInput.tsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
+
 type TextInputComponentProps = {
   label: string;
   name: string;
 };
 
 export default function TextInput({ label, name }: TextInputComponentProps) {
+  const [inputErrorWarning, setInputErrorWarning] = useState(false);
+
   return (
     <label>
       {label}
-      <input type="text" id={name} name={name}></input>
+      <input
+        type="text"
+        id={name}
+        name={name}
+        className={inputErrorWarning ? "error-theme" : ""}
+        onChange={() => setInputErrorWarning(false)}
+      ></input>
     </label>
   );
 }
